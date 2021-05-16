a0f87129cd76e10c04321a326569eedaa17c62af
kaderDiarra <abdel-aziz-kader.diarra@epitech.eu>
2021-05-16 08:40:33 +0200

    change address

--- controllers/client/client.js
+++ controllers/client/client.js
@@ -14,6 +14,7 @@ const projection = {
 exports.getAllClients = async(req, res) => {
     try {
         const clients = await Client.find({}, {...projection})
+        console.log("ENTER !!!!!!!!!!!!!!!!!!")
         console.log(clients)
         res.status(200).send(clients)
     } catch (error) {
